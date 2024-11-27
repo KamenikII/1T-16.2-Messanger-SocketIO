d8f58240e09261e71e9dec617b5c3f21450ebc5c
Ilya <107436210+KamenikII@users.noreply.github.com>
2024-11-27 12:09:17 +0300

    Second try

--- messanger/src/router/index.js
+++ messanger/src/router/index.js
@@ -1,7 +1,7 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 
-import HomeView from './HomeView.vue'
-import AboutView from './AboutView.vue'
+import HomeView from '@views/HomeView.vue'
+import AboutView from '@views/AboutView.vue'
 
 const routes = [
   { 
--- messanger/src/views/HomeVue.vue
+++ messanger/src/views/HomeVue.vue
@@ -1,5 +1,3 @@
 <template>
-  <div class="Home">
-    <h1>This is a home page</h1>
-  </div>
+  
 </template>
